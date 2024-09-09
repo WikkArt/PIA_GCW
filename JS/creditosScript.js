1168c1b6787a4ed114fbf37102349681f32ae81e
WikkArt <fatimita03@outlook.com>
2024-09-09 01:55:23 -0600

    Primer Avance TERMINADO

--- JS/creditosScript.js
+++ JS/creditosScript.js
@@ -1,15 +1,18 @@
 var btnCreadores = document.getElementById("btnCreadores");
 var btnModelos = document.getElementById("btnModelos");
 var btnIconos = document.getElementById("btnIconos");
+var btnHUD = document.getElementById("btnHUD");
 
 var listaCreadores = document.getElementById("listaCreadores");
 var listaModelos = document.getElementById("listaModelos");
 var listaIconos = document.getElementById("listaIconos");
+var listaHUD = document.getElementById("listaHUD");
 
 function mostrarLista(listaParaMostrar) {
     listaCreadores.style.display = 'none';
     listaModelos.style.display = 'none';
     listaIconos.style.display = 'none';
+    listaHUD.style.display = 'none';
 
     listaParaMostrar.style.display = 'flex';
 }
@@ -17,5 +20,6 @@ function mostrarLista(listaParaMostrar) {
 btnCreadores.addEventListener('click', () => mostrarLista(listaCreadores));
 btnModelos.addEventListener('click', () => mostrarLista(listaModelos));
 btnIconos.addEventListener('click', () => mostrarLista(listaIconos));
+btnHUD.addEventListener('click', () => mostrarLista(listaHUD));
 
 mostrarLista(listaCreadores);
